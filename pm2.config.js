57b02334b779da5bb8ca9eee10ce4dd493f1e815
sourav-js <oop.gupta12345@gmail.com>
2022-03-16 23:00:08 +0530

    checked

---
+++ pm2.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps : [{
+      name   : "adminPanel",
+      script : "nodemon grocery.js",
+      error_file : "/home/sourav/sourav/admingroceryJi_error.log",
+      out_file : "/home/sourav/sourav/admingroceryJi_out.log"
+    }]
+  }
